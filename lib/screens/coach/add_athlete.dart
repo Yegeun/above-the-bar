2aab0bcc2a730bd5e0237045360fe94cb1b52477
Yegeun Ji <ukm0613@gmail.com>
2023-03-10 02:13:08 +0000

    Being able to view athles blocs

--- lib/screens/coach/add_athlete.dart
+++ lib/screens/coach/add_athlete.dart
@@ -1,9 +1,13 @@
+import 'package:above_the_bar/bloc/blocs.dart';
+import 'package:above_the_bar/screens/coach_home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
 import 'package:above_the_bar/models/athlete_model.dart';
 import 'package:above_the_bar/bloc/athlete/athlete_bloc.dart';
+
+import 'package:above_the_bar/bloc/program_list/program_list_bloc.dart';
 
 class AddAthlete extends StatefulWidget {
   @override
@@ -12,19 +16,22 @@ class AddAthlete extends StatefulWidget {
 
 class _AddAthleteState extends State<AddAthlete> {
   final TextEditingController _controllerName = TextEditingController();
+
   String get controllerGetNameText => _controllerName.text;
   final TextEditingController _controllerEmail = TextEditingController();
+
   String get controllerGetEmailText => _controllerEmail.text;
   final TextEditingController _controllerBlock = TextEditingController();
+
   String get controllerGetBlock => _controllerBlock.text;
   late DateTime date = DateTime.now();
 
   @override
   void dispose() {
     super.dispose();
-    _controllerName.dispose();
-    _controllerEmail.dispose();
-    _controllerBlock.dispose();
+    _controllerName.clear();
+    _controllerEmail.clear();
+    // context.read<AthleteBloc>().add(LoadAthlete());
   }
 
   @override
@@ -56,14 +63,43 @@ class _AddAthleteState extends State<AddAthlete> {
                   decoration: InputDecoration(hintText: 'Email'),
                 ),
               ),
-              Container(
-                width: 200,
-                margin: EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: _controllerBlock,
-                  decoration: InputDecoration(hintText: 'Block'),
-                ),
-              ),
+              // BlocBuilder<ProgramListBloc, ProgramListState>(
+              //   //TODO this shoudl adpat for uassign and assign
+              //   builder: (context, state) {
+              //     if (state is ProgramListLoading) {
+              //       return Center(
+              //         child: CircularProgressIndicator(),
+              //       );
+              //     }
+              //     if (state is ProgramListLoaded) {
+              //       final List<String> programsList =
+              //           state.programList.toList();
+              //       programsList.insert(0, 'unassigned');
+              //
+              //       String dropdownValue = programsList[0];
+              //       return DropdownButton(
+              //         key: ValueKey(dropdownValue),
+              //         value: dropdownValue,
+              //         items: programsList
+              //             .map((String program) => DropdownMenuItem(
+              //                   value: program,
+              //                   child: Text(program),
+              //                 ))
+              //             .toList(),
+              //         onChanged: (String? newValue) {
+              //           setState(() {
+              //             dropdownValue = newValue!;
+              //             print(dropdownValue);
+              //           });
+              //         },
+              //       );
+              //     } else {
+              //       return Center(
+              //         child: Text('No Programs'),
+              //       );
+              //     }
+              //   },
+              // ),
               Container(
                 width: 200,
                 margin: EdgeInsets.all(8.0),
@@ -94,13 +130,19 @@ class _AddAthleteState extends State<AddAthlete> {
                             AthleteModel(
                               name: controllerGetNameText.toLowerCase(),
                               email: controllerGetEmailText.toLowerCase(),
-                              block: controllerGetBlock.toLowerCase(),
+                              block: '',
                               startDate: DateTime.parse(text),
                             ),
                           ),
                         );
                     dispose();
-                    Navigator.pop(context);
+                    // Navigation.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CoachHome(),
+                      ),
+                    );
                   },
                   child: const Text('Add Athlete'),
                 );
